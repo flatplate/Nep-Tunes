ed7275fdb5fdce9e877d2b8515fc0e1e00bdbe6d
flatplate <uralbayhan0@gmail.com>
2016-07-22 22:12:28 +0300

    changed background and worked on js

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var app = express();
 var bodyParser = require('body-parser');
 var	swig = require('swig');
 var http = require('http').Server(app);
+var fs = require('fs')
 
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+var dir;
+
+fs.readdir("C:\\", function(err,files){
+	dir = files;
+});
+
 http.listen(1997, function(){
-	console.log("Started listening on port " + config.port);
+	console.log("Started listening on port " + 1997);
+});
+app.get('/', function(req,res){
+	res.send(swig.renderFile('./templates/index.html'));
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,16 @@
+var menuItems = ["Video", "Music", "Image"];
+var subMenuMusic = ["Artists", "Albums", "Songs", "Playlists"];
+var subMenuImage = ["Albums", "Images"];
+var activeMenu = "Music";
+var activeSubMusic = "Artists";
+var activeSubImage = "Albums";
+
 $(document).ready(function(){
 	$(".navs li").click(function(){
 		$(".navs li").removeClass("active");
 		$(this).addClass("active");
 		$(".sub-menu div").removeClass("active");
+		activeMenu = $.trim($(this).text());
 	});
 	$(".nav-but1").click(function(){
 		$(".sub-menu-video").addClass("active");
@@ -16,13 +24,16 @@ $(document).ready(function(){
 	$(".sub-menu-music li").click(function(){
 		$(".sub-menu-music li").removeClass("cur");
 		$(this).addClass("cur");
+		activeSubMusic = $(this).text();
 	});
 	$(".sub-menu-image li").click(function(){
 		$(".sub-menu-image li").removeClass("cur");
 		$(this).addClass("cur");
+		activeSubImage = $(this).text();
 	});
 	$(".sub-menu-video li").click(function(){
 		$(".sub-menu-video li").removeClass("cur");
 		$(this).addClass("cur");
+		
 	});
 });
